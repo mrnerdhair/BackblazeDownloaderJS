7dc0ff9023c8dd4a4fbb94416b72c52dcdf07a62
Vojtěch Svědiroh <vojta.svediroh@gmail.com>
2021-03-20 10:35:44 +0100

    Fix a crash while renaming the downloaded file - #7

--- app.js
+++ app.js
@@ -359,8 +359,9 @@ Bluebird.coroutine(function*(emailAddress, password, twoFactorCode, restoreIndex
     streams.showStats();
     console.log("Download done, moving file...");
 
-    yield fileStream.endAsync();
+    fileStream.end();
     yield fs.renameAsync(restoreTmpFilePath, restoreRealFilePath);
 
     console.log("Download complete!");
+    process.exit(0);
 })(process.argv[2], process.argv[3], process.argv[4], process.argv[5], process.argv[6], process.argv[7]).done();
